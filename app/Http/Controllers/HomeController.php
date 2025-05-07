5be12b67e99b76ecfcb6ae69fafcf8657c343d5f
shunsuke <shunsuke-higashi@upload-gp.co.jp>
2025-05-07 15:39:47 +0900

    createのtag機能の追加

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Memo; // Correctly import the Memo model
+use App\Models\Tag; // Correctly import the Tag model
 
 class HomeController extends Controller
 {
@@ -42,16 +43,15 @@ class HomeController extends Controller
     {
         //ログインしているユーザーの情報をviewに渡す
         $user = Auth::user();
-        return view('create', compact('user'));
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        return view('create', compact('user', 'memos'));
     }
 
     //Requwestクラスを使うとホームに入力されたメモやIDをうけとれる
     //POSTメソッドで送信されたデータを受け取る
     public function store(Request $request)
     {
-
         $data = $request->all(); // POSTされたデータを取得
-        // dd($data); // Removed to prevent script termination
         // dd($data);
         // POSTされたデータをDB（memosテーブル）に挿入
         // MEMOモデルにDBへ保存する命令を出す
@@ -59,19 +59,21 @@ class HomeController extends Controller
         // 同じタグがあるか確認
         // $exist_tag = Tag::where('name', $data['tag'])->where('user_id', $data['user_id'])->first();
         // if( empty($exist_tag['id']) ){
-        //     //先にタグをインサート
-        //     $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
+            //先にタグをインサート,タグテーブルにデータを挿入
+            //insertGetIdは、挿入したレコードのIDを取得するためのメソッド(最初はタグのID＝1がとれる)、
+            //とったIDをのちにメモテーブルに入れて、ひもづける
+            $tag_id = Tag::insertGetId(['name' => $data['tag'], 'user_id' => $data['user_id']]);
         // }else{
         //     $tag_id = $exist_tag['id'];
         // }
-        //タグのIDが判明する
+        // タグのIDが判明する
         // タグIDをmemosテーブルに入れてあげる
         // メモをmemosテーブルに挿入し、メモIDを取得
         $memo_id = Memo::insertGetId([
             //カラムとデータが一致している
             'content' => $data['content'], // メモの内容
             'user_id' => $data['user_id'], // ユーザーID
-            // 'tag_id' => $tag_id,           // タグID
+            'tag_id' => $tag_id,           // タグID
             'status' => 1                 // ステータス（1: 有効）
         ]);
 
